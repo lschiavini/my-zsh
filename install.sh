c54286f15acc5c5f130731c26b5cd4a6577dba73
Lucas Schiavini <lucas.s.schiavini@gmail.com>
2022-09-02 13:27:25 -0300

    installing deps and ohmyzsh

---
+++ install.sh
@@ -0,0 +1,8 @@
+
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+sed -i -e "s/.*plugins=(git).*/plugins=(git \n zsh-autosuggestions \n zsh-syntax-highlighting)/" ~/.zshrc
